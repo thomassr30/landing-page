6a4c417a5c61b245acd78f6e3d39b3a884f2c3c4
thomassr30 <thomassoto94@gmail.com>
2021-01-14 14:24:01 -0300

    jumbotron y card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Jumbotron from './Componentes/Jumbotron'
+import Cards from './Componentes/Cards'
 
 function App() {
+
+  const tituloCard = [
+    'Card title 1',
+    'Card title 2',
+    'Card title 3',
+    'Card title 4',
+  ]
+
   return (
     <div className="App">
       <h1>navbar</h1>
-      <div>
-        container
+      <div className="container">
+        <Jumbotron />
+        <Cards tituloCard={tituloCard} />
       </div>
     </div>
   );
