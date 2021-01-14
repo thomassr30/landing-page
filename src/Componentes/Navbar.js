918a7c79c7baf11f44d95b3e2bba776dcc24de87
Jano6elmar <Jano6belmar@gmail.com>
2021-01-14 19:34:08 +0000

    cambios Navbar

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -1,32 +1,45 @@
 import React from "react";
 
-const Navbar = () =>{
+const Navbar = () => {
+    const hidden = () => {
+        const menu = document.querySelector("#navbarResponsive")
+        menu.classList.toggle("none")
+    }
     return (
-        <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
-            
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
-  <button class="navbar-toggler  " type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse " id="navbarText">
-    <ul class="navbar-nav ml-auto">
-      
-      <li class="nav-item active">
-        <h5 class="nav-link" href="#">Home <span class="sr-only">(current)</span></h5>
-      </li>
-      <li class="nav-item">
-        <h5 class="nav-link" href="#">About</h5>
-      </li>
-      <li class="nav-item">
-        <h5 class="nav-link" href="#">Service</h5>
-      </li>
-      <li class="nav-item">
-        <h5 class="nav-link" href="#">Contact</h5>
-      </li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" 
+        type="button" data-toggle="collapse"
+       data-target="#navbarResponsive" 
+       aria-controls="navbarResponsive" 
+       aria-expanded="false" 
+       onClick = {hidden}
+       aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
       </div>
-</nav>
-
+    </div>
+  </nav>
+            
+  
 
 
         
