559b7026934184091eeba10f31ab6640c4cf8994
Jano6elmar <Jano6belmar@gmail.com>
2021-01-14 21:11:26 +0000

    Navbar Final

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import Jumbotron from './Componentes/Jumbotron'
 import Cards from './Componentes/Cards'
 
 function App() {
-  
-  <Navbar titulo="Start Bootstrap" />
-
-
-  const tituloCard = [
+    
+    const tituloCard = [
     'Card title 1',
     'Card title 2',
     'Card title 3',
@@ -16,13 +13,16 @@ function App() {
   ]
 
   return (
+      <>
+     <Navbar titulo="Start Bootstrap" /> 
     <div className="App">
-      <h1>navbar</h1>
+      
       <div className="container">
         <Jumbotron />
         <Cards tituloCard={tituloCard} />
       </div>
     </div>
+    </>
   );
 
 }
